7c51f39d44dc0a7cdbe582cc3f7248659a1c6bcd
Denis Savasteev <doctorzeb8@gmail.com>
2015-05-11 11:42:04 +0800

    add crud urls util

--- src/era/__init__.py
+++ src/era/__init__.py
@@ -1,3 +1,3 @@
 from .utils.functools import just, avg, unidec, emptyless, random_str
 from .utils.translation import _
-from .utils.urls import view_url, package_patterns
+from .utils.urls import view_url, crud_urls, package_patterns
--- src/era/utils/urls.py
+++ src/era/utils/urls.py
@@ -4,20 +4,39 @@ from django.utils.module_loading import import_string
 from .translation import capitalize
 
 
-def view_url(route, args=None, suffix=True):
+def view_url(route, args=None, url_name=None, view_name=None, view_suffix='View'):
     def form_url(package):
         view = import_string(
             '.'.join([
                 package,
                 'views',
                 ''.join([
-                    capitalize(route),
-                    suffix and 'View' or ''])]))
+                    capitalize(view_name or route),
+                    view_suffix])]))
         return url(
             r'^{0}/?$'.format('/'.join(chain([route], args or []))),
             view.as_view(),
-            name=route)
+            name=(url_name or route))
     return form_url
+
+
+def crud_urls(route, object_name):
+    return [
+        view_url(
+            route,
+            args=['add'],
+            url_name='-'.join([object_name, 'add']),
+            view_name=object_name),
+        view_url(route),
+        view_url(
+            route,
+            args=[r'(?P<pk>[0-9]+)'],
+            url_name='-'.join([object_name, 'update']),
+            view_name=object_name),
+        view_url(
+            route,
+            args=['delete'],
+            url_name='-'.join([route, 'delete']))]
 
 
 def package_patterns(package, *urls):
